f815a75671e3ad0b6f26cd2ea2aee08a0781dd69
Roberto <robertofernandes199@hotmail.com>
2022-02-10 22:04:29 -0300

    adcionando typescript e montando servidor node!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("executou!");
+import express, {Request, Response, NextFunction} from 'express';  // express é uma biblioteca agerenciadora de rotas http.
+
+const app = express();
+
+app.get('/status', (req:Request, res:Response, next:NextFunction ) => {    //padrão express para tratamento de rota!
+    res.status(200).send({foo: 'Aplicação automatizada, restartando '});
+
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação escutando a porta 3000!');
+});
+
+//Node
+// Servidor rodando na porta 3000! 
+// fazendo uma requisição... 
